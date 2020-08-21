afbf16d07db918fb35f90b932e47eebd8c7d883b
Waphyyy <69969117+Waphyyy@users.noreply.github.com>
2020-08-21 09:48:22 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const exampleEmbed = new Discord.MessageEmbed();
-const botsettings = require('./botsettings.json');
 
 client.once('ready', () => {
     console.log('Ready!');
